c69904b50ada7458b27a1a8f570424e9f3dfd834
Dana Zack <danaezack@gmail.com>
2023-10-26 15:15:27 -0600

    Write first variable, write first eventlistener, and pseudocode first function

---
+++ main.js
@@ -0,0 +1,25 @@
+// VARIABLES
+var button = document.querySelector(button)
+
+
+// /EVENT LISTENERS
+button.addEventListener('click', displayMessage)
+
+
+//EVENT HANDLERS
+function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+
+
+
+//Write a function to display random mantra & random affirmation
+//Use conditional
+// --> if affirmation selected, then return random from the affirmation array
+// --> if mantra selected, then return random from the mantra array
+// Put return message in place of the svg icon (use "hidden")
+function displayMessage(){
+
+}
+
+// Write hidden/show helper functions?
