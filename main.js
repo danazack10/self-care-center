01d0a3cce60a39bf326ef15046205b6ab9c6e24d
Dana Zack <danaezack@gmail.com>
2023-10-26 15:25:51 -0600

    Add in uncommented instructions for reference

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 // VARIABLES
+//====================================================================
 var button = document.querySelector(button)
 
 
-// /EVENT LISTENERS
+// EVENT LISTENERS
+//====================================================================
 button.addEventListener('click', displayMessage)
 
 
 //EVENT HANDLERS
+//====================================================================
 function getRandomIndex(array) {
     return Math.floor(Math.random() * array.length);
   }
-
-
 
 //Write a function to display random mantra & random affirmation
 //Use conditional
@@ -23,3 +24,17 @@ function displayMessage(){
 }
 
 // Write hidden/show helper functions?
+
+
+
+//====================================================================
+
+
+// CHOSEN TASK: User can delete a message
+// --> Add the ability to delete a messge (ie: when a message shows up, 
+// show a button that says “I don’t like this messge” (or something similar), 
+// and remove it from the list so that it will not show up any more. Make sure
+// to alert the user in some way that the message has been removed.
+// --> NOTE: None of this needs to persist on page refresh, unless you also
+// complete the local storage feature.
+
