e8078bd6af53e5023471306a9e19d64e62ebef1f
Dana Zack <danaezack@gmail.com>
2023-10-28 09:28:08 -0600

    Update syntax per Turing JS style guide

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 // VARIABLES
-var rcvButton = document.querySelector('.receivebutton')
-var img = document.querySelector('.svg')
-var msgCont = document.querySelector('.messagecontainer')
-var affirmRadio = document.querySelector('#affirmation')
-var mantraRadio = document.querySelector('#mantra')
-var rmvButton = document.querySelector('#removebutton')
+var rcvButton = document.querySelector('.receivebutton');
+var img = document.querySelector('.svg');
+var msgCont = document.querySelector('.messagecontainer');
+var affirmRadio = document.querySelector('#affirmation');
+var mantraRadio = document.querySelector('#mantra');
+var rmvButton = document.querySelector('#removebutton');
 
 
 // EVENT LISTENERS
-rcvButton.addEventListener('click', displayMessage)
-rmvButton.addEventListener('click', removeMessage)
+rcvButton.addEventListener('click', displayMessage);
+rmvButton.addEventListener('click', removeMessage);
 
 
 // FUNCTIONS
@@ -18,14 +18,14 @@ function getRandomIndex(array) {
   }
 
 function randomAffirm() {
-  return affirmations[getRandomIndex(affirmations)]
+  return affirmations[getRandomIndex(affirmations)];
 }
 
 function randomMantra() {
-    return mantras[getRandomIndex(mantras)]
+  return mantras[getRandomIndex(mantras)];
 }
 
-function displayMessage(){
+function displayMessage() {
   img.style.display = "none";
   var affirm = randomAffirm();
   var mantra = randomMantra();
@@ -38,22 +38,20 @@ function displayMessage(){
   }
 }
 
-function removeMessage(){
+function removeMessage() {
   event.preventDefault();
-  if (affirmRadio.checked && img.style.display === "none"){
+  if (affirmRadio.checked && img.style.display === "none") {
     for (var i = 0; i < affirmations.length; i++) {
-      if (affirmations[i] === msgCont.innerText){
-        affirmations.splice(i, 1)
-        msgCont.innerText = `Affirmation has been removed!`
-        event.preventDefault();
+      if (affirmations[i] === msgCont.innerText) {
+        affirmations.splice(i, 1);
+        msgCont.innerText = `Affirmation has been removed!`;
       } 
     }
   } else if (mantraRadio.checked && img.style.display === "none") {
     for (var i = 0; i < mantras.length; i++) {
-      if (mantras[i] === msgCont.innerText){
-        mantras.splice(i, 1)
-        msgCont.innerText = `Mantra has been removed!`
-        event.preventDefault();
+      if (mantras[i] === msgCont.innerText) {
+        mantras.splice(i, 1);
+        msgCont.innerText = `Mantra has been removed!`;
       }  
     }
   }
